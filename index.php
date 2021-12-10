516291e02d5051c7b7b2e1f7989bee607c372188
Spectral <bernamn24@hotmail.com>
2021-12-10 12:25:18 -0600

    content

--- index.php
+++ index.php
@@ -9,26 +9,24 @@
     <meta name="theme-color" content="#f43443">
     <title>Bazareño | Empeña &#8226;Compra &#8226;Vende</title>
     <!-- Favicon file -->
-    <link rel="apple-touch-icon" sizes="57x57" href="multimedia/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="multimedia/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="multimedia/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="multimedia/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="multimedia/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="multimedia/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="multimedia/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="multimedia/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="multimedia/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="multimedia/favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="multimedia/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="multimedia/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="multimedia/favicon/favicon-16x16.png">
-    <link rel="shortcut icon" href="multimedia/favicon/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="multimedia/favicon/favicon.ico" type="image/x-icon">
-    <link rel="manifest" href="multimedia/favicon/manifest.json">
+    <link rel="apple-touch-icon" sizes="57x57" href="content/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="content/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="content/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="content/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="content/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="content/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="content/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="content/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="content/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="content/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="content/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="content/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="content/favicon/favicon-16x16.png">
+    <link rel="shortcut icon" href="content/favicon/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="content/favicon/favicon.ico" type="image/x-icon">
+    <link rel="manifest" href="content/favicon/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="https://bazareno.com.mx/wp-content/themes/bazareno/recs/favicon/ms-icon-144x144.png">
-
-
   </head>
   <body>
 
